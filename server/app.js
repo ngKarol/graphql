cc44097587c0a18b4232cd017547f476ede4050e
Karol <->
2021-05-12 00:07:53 +0200

    #9

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const app = express();
+const schema = require('./schema/schema')
 
-app.use('/graph', graphqlHTTP.graphqlHTTP({}));
+
+app.use('/graph', graphqlHTTP.graphqlHTTP({
+    schema
+}));
+
+
+
 app.listen(4000., () => {
     console.log('Listening for requests on port 4000!');
 })
